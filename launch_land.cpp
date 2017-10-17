8a456b90c03549591c974832cbaf2a3c778d7437
leilaskye <31499559+leilaskye@users.noreply.github.com>
2017-10-17 15:30:41 +1000

    Add files via upload

--- launch_land.cpp
+++ launch_land.cpp
@@ -7,145 +7,151 @@
 #include <vector>
 
 
-int acceptable_dist = 0; 
+int acceptable_dist = 0;
 
 int i = 0; // integer in vector
 double dist; // distance between current pos and goal
 double x_pos;
 double y_pos;
 double z_pos;
-bool increment = 0; 
+bool increment = 0;
 bool mission_status = 0;
 bool mission_complete = 0;
 
 //REMOVE THE ARMING CODE TO PREVENT NAV FROM CONSTANTLY TRYING TO ARM
 
 class MavrosGuider {
-	private:
-		ros::NodeHandle nh_;
-		ros::Publisher pub_pose_;
-		ros::Subscriber sub_state_;
-		ros::Subscriber pos_sub; 
-		ros::ServiceClient client_arming_;
-		ros::ServiceClient client_set_mode_;
-		ros::Timer timer_pose_out_;
+        private:
+                ros::NodeHandle nh_;
+                ros::Publisher pub_pose_;
+                ros::Subscriber sub_state_;
+                ros::Subscriber pos_sub;
+                ros::ServiceClient client_arming_;
+                ros::ServiceClient client_set_mode_;
+                ros::Timer timer_pose_out_;
 
-		geometry_msgs::PoseStamped msg_pose_out_;
-		geometry_msgs::PoseStamped msg_current_pose_;
-		mavros_msgs::State msg_current_state_;
-		mavros_msgs::SetMode msg_set_mode_;
+                geometry_msgs::PoseStamped msg_pose_out_;
+                geometry_msgs::PoseStamped msg_current_pose_;
+                mavros_msgs::State msg_current_state_;
+                mavros_msgs::SetMode msg_set_mode_;
                 mavros_msgs::CommandBool msg_arm_cmd_;
-		mavros_msgs::SetMode msg_set_landing_;
+                mavros_msgs::SetMode msg_set_landing_;
 
-		std::string topic_output_pose_;
-		double rate_timer_;
-		std::vector<geometry_msgs::Pose> pos_target; // pose_goal_;
+                std::string topic_output_pose_;
+                double rate_timer_;
+                std::vector<geometry_msgs::Pose> pos_target; // pose_goal_;
 
-	public:
-		MavrosGuider() :
-			nh_( ros::this_node::getName() ),
-			rate_timer_( 20.0 ),
+        public:
+                MavrosGuider() :
+                        nh_( ros::this_node::getName() ),
+                        rate_timer_( 20.0 ),
                         topic_output_pose_( "/mavel/setpoint/position" ) {
 
-			//Get parameters, or if not defined, use the defaults
-			nh_.param( "topic_output_pose", topic_output_pose_, topic_output_pose_ );
+                        //Get parameters, or if not defined, use the defaults
+                        nh_.param( "topic_output_pose", topic_output_pose_, topic_output_pose_ );
 
                         pos_sub = nh_.subscribe<geometry_msgs::PoseStamped>("/vicon/UAVTAQG7/UAVTAQG7", 10, &MavrosGuider::pose_cb, this);
-			sub_state_ = nh_.subscribe<mavros_msgs::State>("/mavros/state", 10, &MavrosGuider::state_cb, this);
-			pub_pose_ = nh_.advertise<geometry_msgs::PoseStamped>( topic_output_pose_, 10 );
-			client_arming_ = nh_.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
-			client_set_mode_ = nh_.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
-			
-			msg_pose_out_.header.frame_id = "world";
-			msg_pose_out_.pose.position.x = 0.0;
-			msg_pose_out_.pose.position.y = 0.0;
-			msg_pose_out_.pose.position.z = 1.0; // was 1 
-			msg_pose_out_.pose.orientation.x = 0.0;
-			msg_pose_out_.pose.orientation.y = 0.0;
-			msg_pose_out_.pose.orientation.z = 0.0;
-			msg_pose_out_.pose.orientation.w = 1.0; // was 1 
+                        sub_state_ = nh_.subscribe<mavros_msgs::State>("/mavros/state", 10, &MavrosGuider::state_cb, this);
+                        pub_pose_ = nh_.advertise<geometry_msgs::PoseStamped>( topic_output_pose_, 10 );
+                        client_arming_ = nh_.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
+                        client_set_mode_ = nh_.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
 
-
-			pos_target.push_back(msg_pose_out_.pose);
-			msg_pose_out_.pose.position.x = 1.0; //edit
-			pos_target.push_back(msg_pose_out_.pose);
-                        msg_pose_out_.pose.position.y = 1.0;
-			pos_target.push_back(msg_pose_out_.pose);
-			msg_pose_out_.pose.position.x = 0.0;  // edit 
-                        pos_target.push_back(msg_pose_out_.pose);
+                        msg_pose_out_.header.frame_id = "map";
+                        msg_pose_out_.pose.position.x = 0.0;
                         msg_pose_out_.pose.position.y = 0.0;
-
+                        msg_pose_out_.pose.position.z = 1.0; // was 1
+                        msg_pose_out_.pose.orientation.x = 0.0;
+                        msg_pose_out_.pose.orientation.y = 0.0;
+                        msg_pose_out_.pose.orientation.z = 0.0;
+                        msg_pose_out_.pose.orientation.w = 1.0; // was 1
                         pos_target.push_back(msg_pose_out_.pose);
 
-                        msg_pose_out_.pose = pos_target[0];
+                        msg_pose_out_.pose.position.x = 1.2; //edit
+                        msg_pose_out_.pose.position.y = 1.2;
+                        pos_target.push_back(msg_pose_out_.pose);
 
-			msg_set_mode_.request.custom_mode = "OFFBOARD";
-			msg_set_landing_.request.custom_mode = "LAND";
-			msg_arm_cmd_.request.value = true; //remove
+                        msg_pose_out_.pose.position.x = -1.2;
+                        msg_pose_out_.pose.position.y = 1.2;
+                        pos_target.push_back(msg_pose_out_.pose);
 
-			ROS_INFO("Waiting for FCU connection...");
+                        msg_pose_out_.pose.position.x = -1.2;
+                        msg_pose_out_.pose.position.y = -1.2;  // edit
+                        pos_target.push_back(msg_pose_out_.pose);
 
-			//Wait for FCU connection
-			while( ros::ok() && !msg_current_state_.connected ){
-				ros::spinOnce();
-				ros::Rate(rate_timer_).sleep();
-			}
+                        msg_pose_out_.pose.position.x = 1.2;
+                        msg_pose_out_.pose.position.y = -1.2;
+                        pos_target.push_back(msg_pose_out_.pose);
 
-			ROS_INFO("FCU detected!");
-			ROS_INFO("Attempting to take control of UAV...");
+                       msg_pose_out_.pose = pos_target[0];
+                       pos_target.push_back(msg_pose_out_.pose);
 
-			//Set up a stamp to keep track of requests, so we don't flood the FCU
-		    ros::Time last_request = ros::Time(0);
+                        msg_set_mode_.request.custom_mode = "OFFBOARD";
+                        msg_set_landing_.request.custom_mode = "LAND";
+                        msg_arm_cmd_.request.value = true; //remove
+
+                        ROS_INFO("Waiting for FCU connection...");
+
+                        //Wait for FCU connection
+                        while( ros::ok() && !msg_current_state_.connected ){
+                                ros::spinOnce();
+                                ros::Rate(rate_timer_).sleep();
+                        }
+
+                        ROS_INFO("FCU detected!");
+                        ROS_INFO("Attempting to take control of UAV...");
+
+                        //Set up a stamp to keep track of requests, so we don't flood the FCU
+                    ros::Time last_request = ros::Time(0);
                     timer_pose_out_ = nh_.createTimer(ros::Duration( 1 / rate_timer_ ), &MavrosGuider::timer_cb, this);
 
-			//Wait for Armed and in OFFBOARD mode
-			while( ros::ok() && ( ( msg_current_state_.mode != "OFFBOARD" ) || ( !msg_current_state_.armed ) ) ) {
+                        //Wait for Armed and in OFFBOARD mode
+                        while( ros::ok() && ( ( msg_current_state_.mode != "OFFBOARD" ) || ( !msg_current_state_.armed ) ) ) {
 
-				if( ( ros::Time::now() - last_request ) > ros::Duration(5.0) ) {
-					if( msg_current_state_.mode != "OFFBOARD" ) {
+                                if( ( ros::Time::now() - last_request ) > ros::Duration(5.0) ) {
+                                        if( msg_current_state_.mode != "OFFBOARD" ) {
                                                 if( client_set_mode_.call(msg_set_mode_) && msg_set_mode_.response.mode_sent ) {
-							ROS_INFO("Offboard mode enabled!");
-						}
+                                                        ROS_INFO("Offboard mode enabled!");
+                                                }
                                         }
 
-					last_request = ros::Time::now();
-				}
+                                        last_request = ros::Time::now();
+                                }
 
-				ros::spinOnce();
-				ros::Rate(rate_timer_).sleep();
-			}
+                                ros::spinOnce();
+                                ros::Rate(rate_timer_).sleep();
+                        }
 
-			ROS_INFO("UAV is in autonomous mode");
+                        ROS_INFO("UAV is in autonomous mode");
                         mission_status = true;
 
-		}
+                }
 
 
-		~MavrosGuider() {
-			//This message won't actually send here, as the node will have already shut down
-			ROS_INFO("Shutting down...");
-		}
+                ~MavrosGuider() {
+                        //This message won't actually send here, as the node will have already shut down
+                        ROS_INFO("Shutting down...");
+                }
 
-		void state_cb( const mavros_msgs::State::ConstPtr& msg_in ) {
-			msg_current_state_ = *msg_in;
-		}
+                void state_cb( const mavros_msgs::State::ConstPtr& msg_in ) {
+                        msg_current_state_ = *msg_in;
+                }
 
-		void pose_cb( const geometry_msgs::PoseStamped msg_in ) {
-			msg_current_pose_ = msg_in;
-			msg_current_pose_.header.stamp = ros::Time::now();
+                void pose_cb( const geometry_msgs::PoseStamped msg_in ) {
+                        msg_current_pose_ = msg_in;
+                        msg_current_pose_.header.stamp = ros::Time::now();
 
                         x_pos = msg_current_pose_.pose.position.x-pos_target[i].position.x;
                         y_pos = msg_current_pose_.pose.position.y-pos_target[i].position.y;
                         z_pos = msg_current_pose_.pose.position.z-pos_target[i].position.z;
-			
-			dist = sqrt((x_pos*x_pos)+(y_pos*y_pos)+(z_pos*z_pos));
 
-			double tolerance = 0.1; //reduce if more accuracy is required
+                        dist = sqrt((x_pos*x_pos)+(y_pos*y_pos)+(z_pos*z_pos));
+
+                        double tolerance = 0.1; //reduce if more accuracy is required
                         if (tolerance < dist){
                                 //ROS_INFO("Travelling to waypoint");
-				acceptable_dist = 0;
-			}
-			else{
+                                acceptable_dist = 0;
+                        }
+                        else{
                                 if(acceptable_dist == 0) {
                                     ROS_INFO("Waypoint reached");
                                 }
@@ -153,21 +159,20 @@ class MavrosGuider {
                                 acceptable_dist++;
                                 //ROS_INFO("Difference to waypoint coord: %d", acceptable_dist);
                         }
-			
+
                         //ROS_INFO("Increment: %d", increment);
 
                         if (acceptable_dist>100){ // if within tolerance for 100 intervals?? // was <
-				acceptable_dist = 0;
-				increment = 1;
+                                acceptable_dist = 0;
+                                increment = 1;
                                 ROS_INFO("Stayed at waypoint long enough");
                                 //ROS_INFO("Increment is now: %d", increment);
-			}
+                        }
 
                         if(increment == 1)
                         {
                             increment = 0;
                             i++;
-
                             if(mission_status)
                             {
                                 if( i < pos_target.size() )
@@ -195,16 +200,15 @@ class MavrosGuider {
 
 
                         }
-                        if(mission_complete == true && msg_current_pose_.pose.position.z<=0.25){
-                            msg_arm_cmd_.request.value = false;
-                            if( client_arming_.call(msg_arm_cmd_) && msg_arm_cmd_.response.success) {
-                                    ROS_INFO("UAV Disarmed!");
-                            }
+                        if(mission_complete == true && msg_current_pose_.pose.position.z<=0.25)
+                        {
+
+                           ROS_INFO("Please disarm UAVTAQG7");
 
                         }
                 }
 
-	// ROS_INFO("Current location: %d", pub_pose_.c_str());
+        // ROS_INFO("Current location: %d", pub_pose_.c_str());
 
                 void timer_cb(const ros::TimerEvent &t_e){
                     msg_pose_out_.header.stamp = ros::Time::now();
@@ -212,10 +216,10 @@ class MavrosGuider {
                 }
 };
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "guider_cpp");
-	MavrosGuider mg;
+        ros::init(argc, argv, "guider_cpp");
+        MavrosGuider mg;
 
-	ros::spin();
+        ros::spin();
 
-	return 0;
+        return 0;
 }
